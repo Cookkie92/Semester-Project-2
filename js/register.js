2c989ac592f1a26f7783565d8f076bb5e48ee36e
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2023-04-22 00:08:56 +0200

    update

--- js/register.js
+++ js/register.js
@@ -1,44 +1,3 @@
-/* <form>
-        <div class="mb-3">
-          <label for="username" class="form-label">Username:</label>
-          <input
-            type="text"
-            class="form-control"
-            id="username"
-            placeholder="Enter username"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="email" class="form-label">Email:</label>
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            placeholder="Enter email"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">Password:</label>
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            placeholder="Enter password"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="confirm-password" class="form-label"
-            >Confirm Password:</label
-          >
-          <input
-            type="password"
-            class="form-control"
-            id="confirm-password"
-            placeholder="Confirm password"
-          />
-        </div>confirm-password
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form> */
 import { baseUrl } from "./settings/api";
 const form = document.querySelector("form");
 const email = document.querySelector("#email");
@@ -62,9 +21,9 @@ function submitForm(event) {
     displayMessage("warning", "invalid values", ".message-container");
   }
   console.log(event);
-  doLogin(usernameValue, emailValue, passwordValue);
+  doRegister(usernameValue, emailValue, passwordValue);
 }
-async function doLogin(username, email, password) {
+async function doRegister(username, email, password) {
   const url = baseUrl + "social/auth/register";
   const data = JSON.stringify({
     name: username,
